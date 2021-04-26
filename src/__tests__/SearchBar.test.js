20ccc55cfc52f37a93371f9d3cb515dd48b2946e
Hanna <hannaeberhoefer@yahoo.com>
2021-04-26 14:24:45 +0100

    Test SearchBar handleTermChange

--- src/__tests__/SearchBar.test.js
+++ src/__tests__/SearchBar.test.js
@@ -1,9 +1,20 @@
 import SearchBar from '../Components/SearchBar/SearchBar.js';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 describe('SearchBar', () => {
+
     it('matches the snapshot', () => {
         const tree = renderer.create(<SearchBar />).toJSON();
         expect(tree).toMatchSnapshot();
     });
+
+    describe ('handleTermChange', () => {
+        it('updates the state', () => {
+            const wrapper = shallow(<SearchBar />);
+            const input = wrapper.find('input');
+            input.simulate('change', { target: { value: 'New Search'} });
+            expect(wrapper.state().term).toEqual('New Search');
+        })
+    })
 });
